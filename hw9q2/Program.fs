3133ce83748396f03bf3eb1c71a21603ec959264
tdb1 <tdb1@williams.edu>
2018-11-16 11:04:30 -0500

    q2 done

--- hw9q2/Program.fs
+++ hw9q2/Program.fs
@@ -1,9 +1,13 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
 open Parser
 open LambdaParser
 open AlphaReduction
+
+/// <summary>Substitutes the expression with_e for v in the expression in_e.</summary>
+/// <param name="v">Variable being substituted</param>
+/// <param name="with_e">Expr being put in place of v</param>
+/// <param name="in_e">Expr being changed</param>
+/// <returns>An Expr</returns>
 let rec sub (v:char) (with_e:Expr) (in_e:Expr) =
  match in_e with
  | Application (e1, e2) -> let x = sub v with_e e1
@@ -16,6 +20,9 @@ let rec sub (v:char) (with_e:Expr) (in_e:Expr) =
                           then (sub v with_e e')
                           else Abstraction(v', (sub v with_e e'))
 
+/// <summary>Applies one Expr to another Expr.</summary>
+/// <param name="e">An Expr.</param>
+/// <returns>A one-step beta reduced Expr.</returns>
 let rec betastep (e: Expr) =
  match e with
  | Variable(v) -> e
@@ -29,10 +36,31 @@ let rec betastep (e: Expr) =
                                                      then Application(e1, (betastep e2))
                                                      else e
 
-let betanorm (e:Expr) =
+/// <summary>Alpha then Beta normalizes the Expr.</summary>
+/// <param name="e">An Expr.</param>
+/// <returns>A reduced Expr.</returns>
+let rec betanorm (e:Expr) =
+
+ let x = (fst (alphanorm e (fv e) Map.empty))
+
+ let rec normalize e1 =
+  let e2 = betastep e1
+  if (e2 = e1)
+  then e2
+  else (normalize e2)
  
+ normalize x 
+
+/// <summary>Parses input to an alpha and beta reduced Expr.</summary>
+/// <param name="input">A string.</param>
+/// <returns>A string with either the reduced expression or message indicating invalid expression.</returns>
+let go input =
+ match parse input with
+ | Some e' -> lambdaprint (betanorm e')
+ | None -> "Invalid Expression"
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+ let input = argv.[0]
+ printfn "%s" (go input)
+ 0
